b15d75b5837d8924da1c8dfd8ee28d26d2afa201
jaleisme <jaleisme.id@gmail.com>
2022-10-21 03:06:33 +0700

    Dashboard Fix

--- pertemuan 6/app.js
+++ pertemuan 6/app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+let task = [
+    {
+        subject: "PAW",
+        task: "Kalkulator",
+        url: "/kalkulator",
+        class: "success", 
+        status: "Done"
+    },
+    {
+        subject: "PAW",
+        task: "TTS",
+        url: "/tts",
+        class: "success", 
+        status: "Done"
+    }
+];
 
 app.listen(3000)
 
 app.get('/', (request, result) => {
-    result.render('index.ejs')
+    result.render('dashboard.ejs', {task:task})
 })
 app.get('/tts', (request, result) => {
     result.render('tts.ejs')
